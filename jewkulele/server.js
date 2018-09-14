b418a5bc19dc1beb4c4824eabc69fd57645dda27
unknown <jordiamir@gmail.com>
2018-09-14 19:44:55 +0300

    server.js modified 12

--- jewkulele/server.js
+++ jewkulele/server.js
@@ -2,14 +2,14 @@ const express =  require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/jewkulele'));
 
 app.listen(process.env.PORT || 8080);
 
 
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/src/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/jewkulele/index.html'));
 })
 
 console.log('Console listening');
