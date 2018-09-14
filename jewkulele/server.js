43923fa366fc20c416a1e0bbbfa4902058c4550c
unknown <jordiamir@gmail.com>
2018-09-14 19:19:57 +0300

    server.js modified 8

--- jewkulele/server.js
+++ jewkulele/server.js
@@ -1,16 +1,33 @@
-const express =  require('express');
-const app = express();
-const path = require('path');
+// const express =  require('express');
+// const app = express();
+// const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
 
 
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/src/index.html'));
+// })
+
+// console.log('Console listening');
+
+var express =  require('express');
+var app = express();
+
+//set port
+var port = process.env.PORT || 8080
+
+app.use(express.static(__dirname + "/dist"));
+
+//routes
+
+app.get("/", function(req, res) {
+    res.render("index");
 })
 
-console.log('Console listening');
-
+app.listen(port, function(){
+    console.log("app running");
+})
