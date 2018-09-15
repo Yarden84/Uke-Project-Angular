b7568b74f8985de20b5429baa102e2bc1f0d8766
unknown <jordiamir@gmail.com>
2018-09-15 05:59:33 +0300

    server.js modified 23

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express =  require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/jewkulele'));
+app.use(express.static(__dirname + '/dist/Jewkulele'));
 
 app.get('/*', (req, res)=> {
-    res.sendFile(path.join(__dirname + '/dist/jewkulele/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Jewkulele/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, ()=>{
