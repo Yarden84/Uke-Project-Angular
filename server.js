888779bb587b5e2e390825684bc2c411b6baccca
unknown <jordiamir@gmail.com>
2018-09-15 05:14:44 +0300

    server.js modified 22

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-// const express =  require('express');
-//     path = require('path');
+const express =  require('express');
+    path = require('path');
 
+const app = express();
+
+app.use(express.static(__dirname + '/dist/jewkulele'));
+
+app.get('/*', (req, res)=> {
+    res.sendFile(path.join(__dirname + '/dist/jewkulele/index.html'));
+});
+
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log('Server started');
+});
+
+// const express =  require('express');
 // const app = express();
+// const path = require('path');
 
 // app.use(express.static(__dirname + '/dist'));
 
-// app.get('/*', (req, res)=> {
-//     res.sendFile(path.join(__dirname + '/dist/src/index.html'));
+// app.listen(process.env.PORT || 8080);
+
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
 // });
-
-// app.listen(process.env.PORT || 8080, ()=>{
-//     console.log('Server started');
-// });
-
-const express =  require('express');
-const app = express();
-const path = require('path');
-
-app.use(express.static(__dirname + '/dist'));
-
-app.listen(process.env.PORT || 8080);
-
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
